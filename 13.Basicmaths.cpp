49d68d194f00fdb806f530be7cb69888225a9a0f
Khushil <108412236+khushil-47@users.noreply.github.com>
2024-01-12 00:00:11 +0530

    Update 13.Basicmaths.cpp

--- 13.Basicmaths.cpp
+++ 13.Basicmaths.cpp
@@ -1,1 +1,12 @@
-
+#include <bits/stdc++.h> 
+long long evenSumTillN(int n) {
+   if (n==0) return 0;
+   long long int sum = 0;
+   for(int i = 0; i<=n; i++){
+       if(i%2==0){
+           sum+=i;
+       }
+   }
+   
+   return sum;
+}
